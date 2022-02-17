7155489107d50e0db557a89fc1374e384eb64aad
ethan <sward2005@126.com>
2022-02-17 23:11:44 +0800

    Try roation but not works.

--- src/qml/imports_js/SceneManager.js
+++ src/qml/imports_js/SceneManager.js
@@ -3,6 +3,7 @@
 
 var lastSlotId =  -1;
 var instances = [];
+var parentMoved = 0;
 function addCar(parent, combinedData, carOffset) {
     var pixelCoordinate = Utils.convertCoordinate(combinedData, carOffset);
 
@@ -33,11 +34,20 @@ function addCar(parent, combinedData, carOffset) {
         });
 
     //instances.push(carObject);
+
     var anim = Qt.createQmlObject ('import QtQuick 2.15; NumberAnimation  { }', slotScene);
     anim.target = carObject;
     anim.property = "z";
-    anim.to = 300;
-    anim.duration = 5000;
+    anim.to = 2000;
+    anim.duration = 10000;
     anim.restart();
+
+//    var roatateAnim = Qt.createQmlObject ('import QtQuick 2.15; NumberAnimation  { }', slotScene);
+//    roatateAnim.target = parent;
+//    roatateAnim.property = "eulerRotation.y";
+//    roatateAnim.to = -90;
+//    roatateAnim.duration = 15000;
+//    roatateAnim.restart();
+
     console.log("Add a car to scene...");
 }
--- src/qml/main.qml
+++ src/qml/main.qml
@@ -12,7 +12,7 @@ import hmi.autoparking 1.0
 //    title: qsTr("autoparking")
     Item {
         id: autoparking
-        width: 800
+        width: 500
         height: 600
         Item {
             id: background
